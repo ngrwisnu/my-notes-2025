4924653ef22bcadb19e3c3389fff07d0890ba94a
ngrwisnu <adiprayogongrwisnu@gmail.com>
2025-01-11 20:15:09 +0700

    add navbar component

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,17 @@
+import { NavLink } from "react-router";
+
+const Navbar = () => {
+  return (
+    <header className="w-full py-4">
+      <div className="container flex justify-between">
+        <div className="text-4xl font-bold">MY-NOTES</div>
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/archive">Archive</NavLink>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
