d0e97f05cfcc0525b97b83e583c8050eb358c98b
ngrwisnu <adiprayogongrwisnu@gmail.com>
2025-01-15 13:52:31 +0700

    add logout button on navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,16 +1,34 @@
+import { LogOut } from "lucide-react";
 import { Link, NavLink } from "react-router";
 
 const Navbar = () => {
+  const logoutHandler = () => {
+    console.log("logout button is clicked");
+  };
+
   return (
     <header className="w-full py-4">
       <div className="container flex justify-between px-4 xl:px-0">
         <div className="text-4xl font-bold">
-          <Link to="/">MY-NOTES</Link>
+          <Link to="/">
+            <img src="/myNotes-dark.png" className="h-[52px]" alt="" />
+          </Link>
         </div>
-        <nav className="space-x-6">
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/archive">Archive</NavLink>
-        </nav>
+        <div className="flex items-center gap-6">
+          <nav className="space-x-6">
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/archive">Archive</NavLink>
+          </nav>
+          <div className="flex gap-4">
+            <div
+              role="button"
+              onClick={logoutHandler}
+              className="flex items-center gap-1 rounded-lg border border-slate-900 px-3 py-2 transition duration-300 hover:cursor-pointer hover:bg-slate-700 hover:text-white"
+            >
+              <LogOut size={20} /> Logout
+            </div>
+          </div>
+        </div>
       </div>
     </header>
   );
